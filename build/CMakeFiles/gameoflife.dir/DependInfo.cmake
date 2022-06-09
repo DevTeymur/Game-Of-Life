a242a25e3de18945dc7f3bf2fe403c5c3f594e89
Teymur <teymurr.601@gmail.com>
2022-06-09 19:28:51 +0400

    CMakeLists.txt but not done

---
+++ build/CMakeFiles/gameoflife.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ufaz/Desktop/Uni Projects/Game-Of-Life/main.c" "/home/ufaz/Desktop/Uni Projects/Game-Of-Life/build/CMakeFiles/gameoflife.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../game"
+  "../gui"
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ufaz/Desktop/Uni Projects/Game-Of-Life/build/game/CMakeFiles/game.dir/DependInfo.cmake"
+  "/home/ufaz/Desktop/Uni Projects/Game-Of-Life/build/gui/CMakeFiles/gui.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- main.c
+++ main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
 
-int main(){
-    
+#include "gui/map.c"
 
-    return 0;
+int main(int argc, char * argv[]){
+	if (SDL_Init(SDL_INIT_VIDEO) > 0) {
+		printf("SDL init failed. Error: %s\n", SDL_GetError());
+	}
+
+	if (IMG_Init(IMG_INIT_PNG) == 0) {
+		printf("IMG init failed. Error: %s\n", SDL_GetError());
+	}
+
+	srand(getpid());
+	
+	int size;
+	printf("Enter the size(which represents the amount of rows and columns) of the board to start: ");
+	scanf("%d", &size);
+
+	Map map = mapCreate(size*3, size*3, size, size);
+
+	startGame(&map);
+
+	return 0;
 }
