26488cab71d3e023d601c87d58776a1341b3334a
NijatAghali <nicat_agali2002@mail.ru>
2022-06-11 23:45:47 +0400

    cunit tests completed

--- CUnitTest/testUnit.c
+++ CUnitTest/testUnit.c
@@ -3,70 +3,53 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdbool.h>
-#include "../game/board.h"
+#include "../game/cell.c"
+#include "../game/board.c"
 
 void testFunc1_1()
 {
-  Cell *cell = cellCreate(0, 0, false);
-  CU_ASSERT_EQUAL(cell, NULL); // must be NULL since it's not alive
+  Cell cell = cellCreate(0, 0, false);
+  CU_ASSERT_EQUAL(cell.row, 0);         // must be 0 as row is equal to zero
+  CU_ASSERT_EQUAL(cell.col, 0);         // must be 0 as column is equal to zero
+  CU_ASSERT_EQUAL(cell.isAlive, false); // must be NULL since it's not alive
 }
 
 void testFunc1_2()
 {
-  Cell *cell = cellCreate(0, 0, true);
-  CU_ASSERT_NOT_EQUAL(cell, NULL); // must be different from 0 since it's alive
+  Cell cell = cellCreate(0, 0, true);
+  CU_ASSERT_NOT_EQUAL(cell.row, 0);        // must be 0 as row is equal to zero
+  CU_ASSERT_NOT_EQUAL(cell.col, 0);        // must be 0 as column is equal to zero
+  CU_ASSERT_NOT_EQUAL(cell.isAlive, true); // must be different from 0 since it's alive
 }
 
 void testFunc2_1()
 {
-  Board *board = boardCreate(0, 0);
-  CU_ASSERT_EQUAL(board, NULL); // must be NULL
+  Board board = boardCreate(0, 0);
+  CU_ASSERT_EQUAL(board.rows, 0); // must be 0 as rows are equal to zero
+  CU_ASSERT_EQUAL(board.cols, 0); // must be 0 as columns are equal to zero
 }
 
 void testFunc2_2()
 {
-  Board *board = boardCreate(3, 1);
-  CU_ASSERT_NOT_EQUAL(board, NULL); // must be different from 0
+  Board board = boardCreate(3, 1);
+  CU_ASSERT_NOT_EQUAL(board.rows, 0); // must be different from zero as rows are not equal to zero
+  CU_ASSERT_NOT_EQUAL(board.cols, 0); // must be different from zero as columns are not equal to zero
 }
 
-void testFunc3_1()
-{
-  int col1, row1;
-  col1 = 33;
-  row1 = 33;
-  Board *board = boardCreate(col1, row1);
-  Board *neighbors[13];
-  int size;
-  size = getNeighbors(board, col1 - 18, row1 - 18, neighbors); // random coordinate on board
-  CU_ASSERT_NOT_EQUAL(size, 0);                                // size must be different from 0
-}
-
-void testFunc3_2()
-{
-  int col1, row1;
-  col1 = 0;
-  row1 = 0;
-  Board *board = boardCreate(col1, row1);
-  Board neighbors[10];
-  int size;
-  size = getNeighbors(board, col1, row1, neighbors); // random coordinate on board
-  CU_ASSERT_EQUAL(size, 0);                          // size must be 0
-}
-void startTest()
+int main()
 {
   CU_initialize_registry();
 
   CU_pSuite suite = CU_add_suite("my_suite", NULL, NULL);
-  CU_pTest t11 = CU_add_test(suite, "my_test", testFunc1_1);
   CU_pTest t12 = CU_add_test(suite, "my_test", testFunc1_2);
+  CU_pTest t11 = CU_add_test(suite, "my_test", testFunc1_1);
   CU_pTest t21 = CU_add_test(suite, "my_test2", testFunc2_1);
   CU_pTest t22 = CU_add_test(suite, "my_test2", testFunc2_2);
-  CU_pTest t31 = CU_add_test(suite, "my_test3", testFunc3_1);
-  CU_pTest t32 = CU_add_test(suite, "my_test3", testFunc3_2);
 
   if (CU_get_error() != CUE_SUCCESS)
     exit(1);
   CU_basic_run_tests();
 
   CU_cleanup_registry();
+  return 0;
 }
